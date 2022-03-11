b8aef7c6f4ff0a71fa2a7a8ab034c27a911d940b
Great Okonkwo <gboyokonkwo35@gmail.com>
2022-03-10 21:11:06 -0500

    Add method to create a message when a user presses send button in discover feed item

--- components/ChatRoomFeed.js
+++ components/ChatRoomFeed.js
@@ -1,14 +1,68 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Text, StyleSheet, SafeAreaView, Button, View, TouchableOpacity, Image, TextInput, FlatList } from 'react-native';
+import * as Keychain from 'react-native-keychain';
+import {productionDomain} from "../networking/api_variables"
+
+
+const fetchItems = async (limit, offset) => {
+    const credential = await Keychain.getInternetCredentials(productionDomain);
+    const token = credential.password;
+    return fetch(`${productionDomain}/chat_rooms?` + new URLSearchParams({offset: offset, limit: limit}), {
+        method: 'Get',
+        headers: {
+            'Content-Type': 'application/json',
+            accept: 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then((response) => {
+        if(response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response.status);
+        }
+    })
+    .then((data) => {
+        return data;
+    })
+
+};
 
 const ChatRoomFeed = () => {
-    return (
-        <View style={styles.mainView}>
-            <FlatList>
-                
-            </FlatList>
-        </View>
-    );
+    const [items, setItems] = useState(null);
+    const itemLimit = 100; 
+return (
+    <SafeAreaView style={styles.mainView}>
+        {
+            useEffect( () => {
+                fetchItems(itemLimit, 0)
+                .then((data) => {
+                    setItems(data);
+                    return (
+                            <FlatList data={items}>
+                                
+                            </FlatList>
+                    );
+                })
+                .catch((error) => {
+                    if(items !== undefined && items !== null && items.length > 0) {
+                        setItems(items.splice(0, items.length));
+                    }
+                    console.log("Rendering out error view");
+                    return (
+                        <View style={{flex: 1, alignSelf: 'center', height: '50%'}}>
+                            <Text>Not Found</Text>
+                        </View>
+                    )
+                    console.log(error);
+                })
+                }
+                , []
+            )
+        }
+    </SafeAreaView>
+)
+
 };
 
 const styles = StyleSheet.create({
@@ -17,7 +71,7 @@ const styles = StyleSheet.create({
         flex: 1
     },
     flatList: {
-
+        flex: 1
     }
 });
 
--- components/DiscoverFeed.js
+++ components/DiscoverFeed.js
@@ -1,9 +1,9 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Text, StyleSheet, SafeAreaView, Button, View, TouchableOpacity, Image, TextInput, ScrollView, FlatList } from 'react-native';
+import { Text, StyleSheet, SafeAreaView, Button, View, TouchableOpacity, Image, TextInput, ScrollView, FlatList, Keyboard, dismissKeyboard, KeyboardAvoidingView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as Keychain from 'react-native-keychain';
-import {productionDomain} from "../networking/api_variables"
+import {productionDomain} from "../networking/api_variables";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
 
@@ -31,26 +31,82 @@ const fetchItems = async (limit, offset) => {
         })
 };
 
-const renderItem = ({  item  }) => {
+const sendMessage = async (item, userId, message) => {
+    const credential = await Keychain.getInternetCredentials(productionDomain);
+    const token = credential.password;
+    return fetch(`${productionDomain}/prompts/${item.prompt.id}/answers/${item.id}/messages`, {
+        method: 'Post',
+        headers: {
+            'Content-Type': 'application/json',
+            accept: 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            user_id: userId,
+            message: message
+        })
+    })
+    .then((response) => {
+        if(response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response.status);
+        }
+    })
+    .then((data) => {return data})
+
+};
+
+const Item = ({item}) => {
+    const [message, setMessage] = useState('');
     const promptBody = item.prompt.body;
     const answerBody = item.body;
+    const user = item.user;
+    const displayName = item.user.display_name;
+    const inputView = useRef();
     return (
-        <View style={{backgroundColor: 'white', paddingVertical: '40%', marginBottom: 10}}>
+        <View style={styles.itemContentView}>
             <View style={styles.stackView}>
+                <View style={styles.profileView}>
+                    <TouchableOpacity style={styles.avatarView}>
+                        <Image defaultSource={require('../assets/images/BlankProfileAsset.png')} source={{uri: 'user.avatar_url', cache: 'force-cache'}} height={styles.avatarView.height} width={styles.avatarView.width}></Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                     <Text style={styles.displayName}>{displayName}</Text>
+                    </TouchableOpacity>
+                </View>
             <Text style={styles.promptText}>{promptBody}</Text>
             <Text style={styles.answerText}>{answerBody}</Text>
             <View style={{marginBottom: 10}}></View>
             <View style={styles.textInputView}>
-            <TextInput style={styles.textInput} placeholder={"Reply..."} multiline={true}>
+            <TextInput ref={inputView} style={styles.textInput} placeholder={"Reply..."} multiline={true} onChangeText={(text) => {
+                setMessage(text);
+            }}>
 
             </TextInput>
-            <TouchableOpacity style={{backgroundColor: '#0581DC', height: 28, width: 28, borderRadius: 15, justifyContent: 'center', alignItems: 'center', alignSelf: 'center', marginRight: 10, flexShrink: 0}}>
+            <TouchableOpacity style={{backgroundColor: '#0581DC', height: 28, width: 28, borderRadius: 15, justifyContent: 'center', alignItems: 'center', alignSelf: 'center', marginRight: 10, flexShrink: 0}} onPress={() => {
+                console.log('Pressed send message button');
+                Keyboard.dismiss(); // dismiss the keyboard
+                inputView.current.clear(); // Clear the input view
+                console.log('This is the message', message);
+                sendMessage(item, user.id, message)
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+            }}>
                 <Icon name={"arrow-upward"} size={15} color={'white'}></Icon>
             </TouchableOpacity>
             </View>
             </View>
         </View>
     )
+};
+
+const renderItem = ({  item  }) => {
+    return <Item item={item}/>
 };
 
 const DiscoverFeed = () => {
@@ -75,7 +131,7 @@ const DiscoverFeed = () => {
     )
     return (
         <SafeAreaView style={styles.mainView}>
-            <FlatList ref={flatlist} style={styles.flatList} data={items} keyExtractor={(item) => item.id} refreshing={false} onRefresh={() => {
+            <FlatList ref={flatlist} style={styles.flatList} data={items} keyExtractor={(item) => item.id} refreshing={false} inverted={false} keyboardShouldPersistTaps='handled' keyboardDismissMode='on-drag' onRefresh={() => {
                 flatlist.refreshing = true 
                 fetchItems(itemLimit, 0)
                 .then((data) => {
@@ -116,7 +172,7 @@ const styles = StyleSheet.create({
         flex: 1
     },
     itemContentView: {
-
+        backgroundColor: 'white', paddingVertical: '40%', marginBottom: 10
     },
     stackView: {
         marginHorizontal: 37,
@@ -146,6 +202,24 @@ const styles = StyleSheet.create({
     },
     textInputView: {
         backgroundColor: '#F0F0F0', borderRadius: 20, width: '95%', alignSelf: 'flex-start', alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between'
+    },
+    profileView: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        marginBottom: 19,
+        alignItems: 'center'
+    },
+    avatarView: {
+        backgroundColor: "#F0F0F0",
+        height: 40,
+        width: 40,
+        borderRadius: 20,
+        marginRight: 10
+    },
+    displayName: {
+        fontFamily: 'Roboto-Regular',
+        fontSize: 15,
+        fontWeight: 'normal'
     }
 });
 
